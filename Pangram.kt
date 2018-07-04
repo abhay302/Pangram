73be31c9c0fc0402b3f187500976790bae0daf75
abhay302 <39900609+abhay302@users.noreply.github.com>
2018-07-04 16:52:26 +0530

    Update Pangram.kt

--- Pangram.kt
+++ Pangram.kt
@@ -1,7 +1,7 @@
 /**
  * isPangram() is an extension function which returns true if the given string is a pangram and returns false if the string is not a pangram
  */
-fun  String.isPangram() = filter { it.isLetter() }.map { it.toLowerCase() }.toSet().size==26
+fun  String.isPangram() = filter { it.isLetter() }.map { it.toLowerCase() }.toSet().size == 26
 
 fun main(args: Array<String>) {
     print("The quick brown fox jumps over the lazy dog.".isPangram())
